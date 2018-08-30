214cde0425c7151914c18283ec6bb594c866f582
William Moreno <williammorenoprieto@gmail.com>
2018-08-30 12:23:45 -0500

    TESTfinal

--- EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Filtrarevento.java
+++ EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Filtrarevento.java
@@ -1,13 +1,27 @@
 package com.example.carlosrestrepo.entregaicm1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Filtrarevento extends AppCompatActivity {
 
+    Button filev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrarevento);
+
+        filev=(Button) findViewById(R.id.butfilev);
+
+        filev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Eventopricipal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Filtrarnoticias.java
+++ EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Filtrarnoticias.java
@@ -1,13 +1,27 @@
 package com.example.carlosrestrepo.entregaicm1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Filtrarnoticias extends AppCompatActivity {
 
+    Button filnot;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtrarnoticias);
+
+        filnot=(Button) findViewById(R.id.butfilnot);
+
+        filnot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(),Eventopricipal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
