d00271f15c9aca98548bb8609ab686f5dac5a27f
William Moreno <williammorenoprieto@gmail.com>
2018-08-30 13:09:29 -0500

    TESTFinalFinal
    Co-Authored-By: Miguel Gutiérrez <miguelgutierrezi@users.noreply.github.com>

--- EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Anfitrion.java
+++ EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/Anfitrion.java
@@ -12,7 +12,7 @@ public class Anfitrion extends AppCompatActivity {
     private Button editar;
     private Button agregar;
     private Button logout;
-    private ListView lugares;
+    private Button lugares;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,62 +20,38 @@ public class Anfitrion extends AppCompatActivity {
 
         editar = (Button) findViewById(R.id.button3);
         editar.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                editar();
-
+                Intent intent = new Intent(view.getContext(), EditarPerfilAnfitrion.class);
+                startActivity(intent);
             }
         });
 
         agregar = (Button) findViewById(R.id.button4);
         agregar.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                agregar();
-
+                Intent intent = new Intent(view.getContext(), AgregarAlojamiento.class);
+                startActivity(intent);
             }
         });
 
         logout = (Button) findViewById(R.id.button2);
         logout.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                logout();
-
+                Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                startActivity(intent);
             }
         });
 
-        lugares = (ListView) findViewById(R.id._dynamic);
+        lugares = (Button) findViewById(R.id.button11);
         lugares.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                lugares();
-
+                Intent intent = new Intent(view.getContext(), VerLugar.class);
+                startActivity(intent);
             }
         });
-    }
-
-    public void editar(){
-        Intent intent = new Intent(this, EditarPerfilAnfitrion.class);
-        startActivity(intent);
-    }
-
-    public void agregar(){
-        Intent intent = new Intent(this, AgregarAlojamiento.class);
-        startActivity(intent);
-    }
-
-    public void logout(){
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-    public void lugares(){
-        Intent intent = new Intent(this, VerLugar.class);
-        startActivity(intent);
     }
 }
