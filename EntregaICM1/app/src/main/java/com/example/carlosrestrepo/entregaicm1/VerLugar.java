32eb324cf3a5f56d6c71d19eb59e087e6c3ea045
William Moreno <williammorenoprieto@gmail.com>
2018-08-30 11:18:56 -0500

    anfitr
    Co-Authored-By: Miguel Gutiérrez <miguelgutierrezi@users.noreply.github.com>

---
+++ EntregaICM1/app/src/main/java/com/example/carlosrestrepo/entregaicm1/VerLugar.java
@@ -0,0 +1,13 @@
+package com.example.carlosrestrepo.entregaicm1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerLugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_lugar);
+    }
+}
